127b8e54723e84e4aea43acd652be89d7f3ae923
Olzhas Askar <olzhas.askar@gmail.com>
2020-06-10 23:52:31 +0200

    Vim Be Good

---
+++ vim/plugins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/rakr/vim-one.git ~/.vim/pack/vim-one/start/vim-one
+git clone https://github.com/tpope/vim-commentary.git ~/.vim/pack/tpope/start/commentary
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/tpope/start/fugitive
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
+git clone https://github.com/neoclide/coc.nvim.git ~/.vim/pack/coc/start/coc.nvim
+
---
+++ vim/shortcuts.md
@@ -0,0 +1,11 @@
+Single number on a line:
+
+increment <C-a> decrement <C-x>
+
+Save and quit
+:wq == ZZ
+
+Toggle case
+~
+
+

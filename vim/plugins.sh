7720aeb3b7787cc9f3f35be3e2d93217178b9c28
pheeria <olzhas.askar@deliveryhero.com>
2020-07-02 19:54:53 +0200

    Add FZF

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -5,4 +5,5 @@ git clone https://github.com/tpope/vim-commentary.git ~/.vim/pack/tpope/start/co
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/tpope/start/fugitive
 git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
 git clone https://github.com/neoclide/coc.nvim.git ~/.vim/pack/coc/start/coc.nvim
+git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/fzf/start/fzf
 

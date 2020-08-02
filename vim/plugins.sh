0f0755a0230b9a2be0a90524314332ed5b50f2f0
pheeria <olzhas.askar@deliveryhero.com>
2020-08-02 12:56:06 +0200

    Restructure plugins

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/rakr/vim-one.git ~/.vim/pack/vim-one/start/vim-one
+git clone https://github.com/rakr/vim-one.git ~/.vim/pack/themes/start/one
+git clone https://github.com/arcticicestudio/nord-vim.git ~/.vim/pack/themes/start/nord
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/themes/start/lightline
+git clone https://github.com/colepeters/spacemacs-theme.vim.git ~/.vim/pack/themes/start/spacemacs
+
 git clone https://github.com/tpope/vim-commentary.git ~/.vim/pack/tpope/start/commentary
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/tpope/start/fugitive
-git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
-git clone https://github.com/neoclide/coc.nvim.git ~/.vim/pack/coc/start/coc.nvim
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/fzf/start/fzf
-git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/lightline/start/lightline
-git clone https://github.com/mbbill/undotree.git ~/.vim/pack/undotree/start/undotree
+git clone https://github.com/tpope/vim-projectionist.git ~/.vim/pack/tpope/start/projectionist
+git clone https://github.com/tpope/vim-vinegar.git ~/.vim/pack/tpope/start/vinegar
+
+git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/junegunn/start/fzf
+git clone https://github.com/junegunn/vim-peekaboo.git ~/.vim/pack/junegunn/start/peekaboo
+
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/plugins/start/typescript
+git clone https://github.com/neoclide/coc.nvim.git ~/.vim/pack/plugins/start/coc
+git clone https://github.com/mbbill/undotree.git ~/.vim/pack/plugins/start/undotree
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki

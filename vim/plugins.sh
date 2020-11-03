d2a3a5e26089221b187a7d3057acae22881ced78
pheeria <olzhas.askar@deliveryhero.com>
2020-11-03 17:39:33 +0100

    More plugins

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -2,18 +2,24 @@
 
 git clone https://github.com/rakr/vim-one.git ~/.vim/pack/themes/start/one
 git clone https://github.com/arcticicestudio/nord-vim.git ~/.vim/pack/themes/start/nord
-git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/themes/start/lightline
-git clone https://github.com/colepeters/spacemacs-theme.vim.git ~/.vim/pack/themes/start/spacemacs
+git clone https://github.com/itchyny/lightline.vim.git ~/.vim/pack/themes/start/lightline
+git clone https://github.com/embark-theme/vim.git ~/.vim/pack/themes/start/embark
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/themes/start/gruvbox
 
 git clone https://github.com/tpope/vim-commentary.git ~/.vim/pack/tpope/start/commentary
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/tpope/start/fugitive
 git clone https://github.com/tpope/vim-projectionist.git ~/.vim/pack/tpope/start/projectionist
 git clone https://github.com/tpope/vim-vinegar.git ~/.vim/pack/tpope/start/vinegar
+git clone https://github.com/tpope/vim-surround.git ~/.vim/pack/tpope/start/surround
+git clone https://github.com/tpope/vim-repeat.git ~/.vim/pack/tpope/start/repeat
 
 git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/junegunn/start/fzf
 git clone https://github.com/junegunn/vim-peekaboo.git ~/.vim/pack/junegunn/start/peekaboo
 
-git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/plugins/start/typescript
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/syntax/start/typescript
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/syntax/start/javascript
+git clone https://github.com/MaxMEllon/vim-jsx-pretty.git ~/.vim/pack/syntax/start/jsx
+
 git clone https://github.com/neoclide/coc.nvim.git ~/.vim/pack/plugins/start/coc
 git clone https://github.com/mbbill/undotree.git ~/.vim/pack/plugins/start/undotree
-git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/wiki

0b8185b0011c79149f10e86ee28fc0c30c01822d
pheeria <olzhas.askar@deliveryhero.com>
2021-07-16 17:01:19 +0200

    feat: whoop! whoop!

---
+++ init.lua
@@ -0,0 +1,389 @@
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true, silent = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Install packer
+local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.api.nvim_exec(
+  [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]],
+  false
+)
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'tpope/vim-fugitive' -- Git commands in nvim
+  use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
+  use 'tpope/vim-projectionist' -- Alternate files
+  use 'tpope/vim-surround' -- Alternate files
+  use 'tpope/vim-repeat' -- Alternate files
+  -- UI to select things (files, grep results, open buffers...)
+  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  -- Additional textobjects for treesitter
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'kabouzeid/nvim-lspinstall' -- Automatically install LSP servers
+  use 'hrsh7th/nvim-compe' -- Autocompletion
+  use 'vim-test/vim-test' -- Run all my tests
+  use 'fenetikm/falcon' -- Colorscheme
+  use 'glacambre/firenvim' -- Neovim in browser!
+  use 'kyazdani42/nvim-web-devicons' -- Tree Icons
+  use 'kyazdani42/nvim-tree.lua' -- Tree with collapsed empty folders
+end)
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.swapfile = false
+opt.emoji = false
+opt.splitbelow = true
+opt.splitright = true
+opt.signcolumn = 'number'
+
+--Incremental live completion
+opt.inccommand = 'nosplit'
+
+--Set highlight on search
+opt.hlsearch = false
+
+--Make line numbers default
+vim.wo.number = true
+
+--Do not save when switching buffers
+opt.hidden = true
+
+--Enable mouse mode
+opt.mouse = 'a'
+
+--Enable break indent
+opt.breakindent = true
+
+--Save undo history
+opt.undofile = true
+
+--Case insensitive searching UNLESS /C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+--Decrease update time
+opt.updatetime = 250
+
+--Set colorscheme (order is important here)
+opt.termguicolors = true
+g.colors_name = 'falcon'
+
+--Remap space as leader key
+g.mapleader = ' '
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+--Add leader shortcuts
+map('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
+map('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]])
+map('n', '<leader>g', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]])
+
+-- LSP settings
+-- keymaps
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>le', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "<space>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("n", "<space>cf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+end
+
+-- Configure lua language server for neovim development
+local lua_settings = {
+  Lua = {
+    runtime = {
+      -- LuaJIT in the case of Neovim
+      version = 'LuaJIT',
+      path = vim.split(package.path, ';'),
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'},
+    },
+    telemetry = {
+        enable = false
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = {
+        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      },
+    },
+  }
+}
+
+-- config that activates keymaps and enables snippet support
+local function make_config()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  return {
+    -- enable snippet support
+    capabilities = capabilities,
+    -- map buffer local keybindings when the language server attaches
+    on_attach = on_attach,
+  }
+end
+
+-- lsp-install
+local function setup_servers()
+  require'lspinstall'.setup()
+
+  -- get all installed servers
+  local servers = require'lspinstall'.installed_servers()
+
+  for _, server in pairs(servers) do
+    local config = make_config()
+
+    -- language specific config
+    if server == "lua" then
+      config.settings = lua_settings
+    end
+
+    require'lspconfig'[server].setup(config)
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
+-- replace the default lsp diagnostic letters with prettier symbols
+fn.sign_define("LspDiagnosticsSignError", {text = "🔴", numhl = "LspDiagnosticsDefaultError"})
+fn.sign_define("LspDiagnosticsSignWarning", {text = "🟡", numhl = "LspDiagnosticsDefaultWarning"})
+fn.sign_define("LspDiagnosticsSignInformation", {text = "🔵", numhl = "LspDiagnosticsDefaultInformation"})
+fn.sign_define("LspDiagnosticsSignHint", {text = "🟢", numhl = "LspDiagnosticsDefaultHint"})
+
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+        ['al'] = '@loop.outer',
+        ['il'] = '@loop.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+}
+
+-- Set completeopt to have a better completion experience
+opt.completeopt = 'menuone,noinsert'
+
+-- Compe setup
+require('compe').setup {
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  resolve_timeout = 800,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  },
+  source = {
+    path = true,
+    buffer = true,
+    calc = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    vsnip = false,
+    ultisnips = false,
+    luasnip = false,
+  },
+}
+
+-- Utility functions for compe
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = fn.col '.' - 1
+  if col == 0 or fn.getline('.'):sub(col, col):match '%s' then
+    return true
+  else
+    return false
+  end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+
+_G.tab_complete = function()
+  if fn.pumvisible() == 1 then
+    return t '<C-n>'
+  elseif check_back_space() then
+    return t '<Tab>'
+  else
+    return fn['compe#complete']()
+  end
+end
+
+_G.s_tab_complete = function()
+  if fn.pumvisible() == 1 then
+    return t '<C-p>'
+  else
+    return t '<S-Tab>'
+  end
+end
+
+-- Map tab to the above tab complete functiones
+map('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+map('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+map('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+map('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+
+-- Map compe confirm and complete functions
+map('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
+map('i', '<c-space>', 'compe#complete()', { expr = true })
+
+map('n', '<leader>vc', ':vs ~/.config/nvim/init.lua<cr>')
+
+
+-- Terminal, behave like in Vim!
+vim.api.nvim_exec(
+  [[
+  augroup Terminal
+    autocmd!
+    autocmd TermOpen * call feedkeys("i")
+    autocmd TermClose * call feedkeys("i")
+  augroup end
+]],
+  false
+)
+
+
+-- Setup Nvim Tree
+map('n', '<leader>e', ':NvimTreeToggle<cr>')
+g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+g.nvim_tree_group_empty = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_gitignore = 1
+g.nvim_tree_auto_open = 1
+g.nvim_tree_width = 40
+
+-- Vim Test
+map('n', '<leader>t', ':w<CR> :TestFile<CR>')
+cmd [[
+  let test#strategy = 'neovim'
+  let test#java#runner = 'gradletest'
+  let test#scala#runner = 'sbttest'
+  let g:test#javascript#runner = 'jest'
+  let g:test#preserve_screen = 1
+]]

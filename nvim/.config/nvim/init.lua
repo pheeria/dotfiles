cceb347d5badf7ad58ae974f9616ce7c7d64798b
pheeria <olzhas.askar@deliveryhero.com>
2022-11-16 00:16:38 +0100

    feat: nvim-cmp set up

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -40,6 +40,9 @@ packer.startup(function()
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'hrsh7th/cmp-nvim-lsp' -- Default capabilities (?!)
+  use 'hrsh7th/nvim-cmp' -- Autocompletion
+  use 'PaterJason/cmp-conjure' -- Conjure Autocompletion
   use 'fenetikm/falcon' -- Colorscheme
   use 'Olical/conjure' -- Clojure REPL
 end)
@@ -143,10 +146,60 @@ local on_attach = function(client, bufnr)
   end
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
+local cmp = require('cmp')
 
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { 'i', 's' }),
 
+    ['<S-Tab>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'conjure' },
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require('lspconfig')
+lspconfig.clojure_lsp.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+lspconfig.pyright.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
 
 -- replace the default lsp diagnostic letters with prettier symbols
 local signs = { Error = '🔴', Warn = '🟡', Hint = '🔵', Info = '🟢' }
@@ -232,7 +285,7 @@ cmd(
 cmd [[
   augroup ConjureRemoveSponsor
     autocmd!
-    autocmd BufWinEnter "conjure-log-*" "silent s/; Sponsored by @.*//e"
+    autocmd BufWinEnter "conjure-log-*" "silent s/Sponsored//e"
   augroup end
 ]]
 

bc192a8085f542bbe4fbfe75403503424efee6ab
pheeria <olzhas.askar@deliveryhero.com>
2022-11-16 00:14:10 +0100

    fix: nvim-compe is discontinued 🤦

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -40,7 +40,6 @@ packer.startup(function()
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/nvim-compe' -- Autocompletion
   use 'fenetikm/falcon' -- Colorscheme
   use 'Olical/conjure' -- Clojure REPL
 end)
@@ -248,86 +247,6 @@ require('nvim-treesitter.configs').setup {
 
 -- Set completeopt to have a better completion experience
 opt.completeopt = 'menuone,noinsert'
-
--- Compe setup
-require('compe').setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 1,
-  preselect = 'enable',
-  throttle_time = 80,
-  source_timeout = 200,
-  resolve_timeout = 800,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-    max_width = 120,
-    min_width = 60,
-    max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  },
-  source = {
-    path = true,
-    buffer = true,
-    calc = true,
-    nvim_lsp = true,
-    nvim_lua = true,
-    vsnip = false,
-    ultisnips = false,
-    luasnip = false,
-  },
-}
-
--- Utility functions for compe
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = fn.col '.' - 1
-  if col == 0 or fn.getline('.'):sub(col, col):match '%s' then
-    return true
-  else
-    return false
-  end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-
-_G.tab_complete = function()
-  if fn.pumvisible() == 1 then
-    return t '<C-n>'
-  elseif check_back_space() then
-    return t '<Tab>'
-  else
-    return fn['compe#complete']()
-  end
-end
-
-_G.s_tab_complete = function()
-  if fn.pumvisible() == 1 then
-    return t '<C-p>'
-  else
-    return t '<S-Tab>'
-  end
-end
-
--- Map tab to the above tab complete functiones
-map('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-map('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-map('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-map('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-
--- Map compe confirm and complete functions
-map('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-map('i', '<c-space>', 'compe#complete()', { expr = true })
 
 map('n', '<leader>vc', ':vs ~/.config/nvim/init.lua<cr>')
 map('n', '<leader>nt', ':tabnew<CR>:terminal<CR>')

5849b5dd0c7664691d39dbab0cf4d9d2408cd776
Olzhas Askar <olzhas.askar@gmail.com>
2023-10-13 11:23:41 +0200

    feat: gcln 🧹

--- zsh/.config/zsh/lib/git.zsh
+++ zsh/.config/zsh/lib/git.zsh
@@ -191,3 +191,22 @@ function git_main_branch() {
 }
 
 alias gswm='git switch $(git_main_branch)'
+alias gcln='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
+
+HASH="%C(always,yellow)%h%C(always,reset)"
+RELATIVE_TIME="%C(always,green)%ar%C(always,reset)"
+AUTHOR="%C(always,bold blue)%an%C(always,reset)"
+REFS="%C(always,red)%d%C(always,reset)"
+SUBJECT="%s"
+
+FORMAT="$HASH $RELATIVE_TIME{$AUTHOR{$REFS $SUBJECT"
+
+function pretty_git_log() {
+  git log --graph --pretty="tformat:$FORMAT" $* |
+  column -t -s '{' |
+  less -XRS --quit-if-one-screen
+}
+
+function remove_untracked_files() {
+  git ls-files --other --exclude-standard | xargs rm -rf
+}

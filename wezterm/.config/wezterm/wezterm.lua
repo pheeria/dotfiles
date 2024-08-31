9c3ab846e1010174dc451c0cc8fd9faaa1415252
Olzhas Askar <olzhas.askar@gmail.com>
2024-08-31 20:44:20 +0200

    feat: adjusting for linux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "AtelierSulphurpool"
+-- config.color_scheme = "AtelierSulphurpool"
 config.initial_rows = 40
 config.initial_cols = 120
 config.font = wezterm.font("Hermit")
@@ -13,7 +13,7 @@ config.use_dead_keys = true
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.adjust_window_size_when_changing_font_size = false
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_padding = {
     -- top = "1.5 cell",

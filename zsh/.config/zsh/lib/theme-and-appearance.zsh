5c3ca983e882151a1b2cc776686f0446085fd888
Olzhas Askar <olzhas.askar@gmail.com>
2026-06-03 20:42:35 +0200

    fix: trim theming

--- zsh/.config/zsh/lib/theme-and-appearance.zsh
+++ zsh/.config/zsh/lib/theme-and-appearance.zsh
@@ -73,10 +73,3 @@ esac
 
 unfunction test-ls-args
 
-# git theming default: Variables for theming the git info prompt
-ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
-ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
-ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
-ZSH_THEME_RUBY_PROMPT_PREFIX="("
-ZSH_THEME_RUBY_PROMPT_SUFFIX=")"

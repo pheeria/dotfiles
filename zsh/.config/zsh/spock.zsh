6fd21e2d6bcff407ad6bdcdbaf30fae427fed74b
Olzhas Askar <olzhas.askar@gmail.com>
2026-06-04 11:31:08 +0200

    fix: indicate a remote shell

--- zsh/.config/zsh/spock.zsh
+++ zsh/.config/zsh/spock.zsh
@@ -16,9 +16,15 @@ function get_pwd() {
 return_status="%(?:🖖:🤨)"
 prompt_suffix="%{$fg[cyan]%}❯%{$reset_color%} "
 
+if [[ -n $CODESPACES ]]; then
+  remote_indicator="%{$fg[red]%}[cs]%{$reset_color%} "
+else
+  remote_indicator=""
+fi
+
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT=' ${return_status} %{$fg[cyan]%}$(get_pwd)%{$reset_color%} $(git_prompt_info)${prompt_suffix}'
+PROMPT=' ${remote_indicator}${return_status} %{$fg[cyan]%}$(get_pwd)%{$reset_color%} $(git_prompt_info)${prompt_suffix}'
